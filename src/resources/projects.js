5285464cc708e1034cf4d50ed848ddd13d39de2a
Paula Rinaldi <paurinaldi95@gmail.com>
2022-05-08 15:56:09 -0300

    TG: Added route to add project

--- src/resources/projects.js
+++ src/resources/projects.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const fileSystem = require('fs');
 const projects = require('../data/projects.json');
 
 const router = express.Router();
 
-router.get('/getById/:id', (req, res) => {
+router.get('/getProjectById/:id', (req, res) => {
   const projectId = req.params.id;
   const project = projects.find((p) => p.id === projectId);
   if (project) {
@@ -12,5 +13,17 @@ router.get('/getById/:id', (req, res) => {
     res.send('Project not found');
   }
 });
+
+router.post("/addProject", (req, res) => {
+    const projectData = req.body;
+    projects.push(projectData);
+    fileSystem.writeFile("src/data/projects.json", JSON.stringify(projects), (err) => {
+        if(err){
+            res.send(err)
+        } else {
+            res.send("Project created")
+        }
+    })
+})
 
 module.exports = router;
