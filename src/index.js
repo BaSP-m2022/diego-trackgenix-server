ee2375dfb8b012eeb81b304a597677312e0f40bb
Gabriel Vazquez <gavf.vazquez@gmail.com>
2022-05-09 18:56:53 -0300

    TG: The function that update a admin was done

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ const adminsRouter = require('./resources/admins');
 
 const app = express();
 const port = process.env.PORT || 3000;
-
 app.use(express.json());
 
 app.use('/admins', adminsRouter);
--- src/resources/admins.js
+++ src/resources/admins.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 
 const router = express.Router();
 const adminsList = require('../data/admins.json');
@@ -11,13 +12,40 @@ router.get('/', (req, res) => {
     const filteredAdmins = adminsList.filter((admin) => {
       let isValid = true;
       arrayOfFiltersValue.forEach((key) => {
-        isValid = (JSON.stringify(admin[key])).toLowerCase() === objectOfFilters[key].toLowerCase();
+        if (req.query.active) {
+          isValid = (JSON.stringify(admin[key])).toLowerCase()
+                        === objectOfFilters[key].toLowerCase();
+        } else {
+          isValid = admin[key] === objectOfFilters[key];
+        }
       });
       return isValid;
     });
     res.send(filteredAdmins);
   } else {
     res.send(adminsList);
+  }
+});
+
+router.put('/:id', (req, res) => {
+  const adminId = req.params.id;
+  const adminData = req.body;
+  const adminRequired = adminsList.find((admin) => adminId === admin.id);
+  if (adminRequired) {
+    adminRequired.first_name = adminData.first_name;
+    adminRequired.last_name = adminData.last_name;
+    adminRequired.email = adminData.email;
+    adminRequired.gender = adminData.gender;
+    adminRequired.active = adminData.active;
+    fs.writeFile('src/data/admins.json', JSON.stringify(adminsList), (err) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send('The admin list was updated');
+      }
+    });
+  } else {
+    res.send('The id of the admin is invalid');
   }
 });
 
