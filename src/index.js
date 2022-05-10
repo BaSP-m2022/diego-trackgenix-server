176a05c1b88c848726d568937eb3ff62d075637e
OctavioRodriguezPaseyro <101280747+OctavioRodriguezPaseyro@users.noreply.github.com>
2022-05-10 16:01:05 -0300

    TG: timesheet GET, POST, PUT and DELETE

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
-// use "import" to import libraries
-import express from "express";
+// use 'import' to import libraries
+import express from 'express';
 
-// use "require" to import JSON files
-const admins = require("./data/admins.json");
+// use 'require' to import JSON files
+import admins from './data/admins.json';
+import timesheets from './data/time-sheets.json';
+import timesheetRouter from './resources/time-sheets';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use('/timesheets', timesheetRouter);
 
-app.get("/", async (req, res) => {
-  res.send("Hello World!");
+// app.get('/', async (req, res) => {
+//   res.send('Hello World!');
+// });
+
+app.get('/timesheets', (req, res) => {
+  res.status(200).json({
+    data: timesheets,
+  });
 });
 
-app.get("/admins", (req, res) => {
+app.get('/admins', (req, res) => {
   res.status(200).json({
     data: admins,
   });
---
+++ src/resources/time-sheets.js
@@ -0,0 +1,71 @@
+import express from 'express';
+import fs from 'fs';
+import timesheets from '../data/time-sheets.json';
+const router = express.Router();
+
+
+router.get('/', (req, res) => {
+    if (req.query) {
+        const filter = req.query;
+        const filers = Object.keys(req.query);
+        const filteredTimesheets = timesheets.filter((timesheet) => {
+        let isValid = true;
+        filders.forEach((key) => {
+            console.log(key, timesheet[key], filter[key]);
+            isValid = isValid && timesheet[key] === filter[key];
+        });
+        return isValid;
+    });
+    res.send(filteredTimesheets);
+    } else {
+        res.send(timesheets);
+    }
+});
+
+router.post('/', (req, res) => {
+    const timesheetData = req.body;
+    timesheets.push(timesheetData);
+    fs.writeFile('src/data/time-sheets.json', JSON.stringify(timesheets), (err) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send('Timesheet created');
+        }
+    });
+});
+
+router.put('/:id', (req, res) => {
+    const timesheetUpdatedData = req.body;
+    const timesheetsUpdated = timesheets.map((t) => {
+        if (t.id === req.params.id) {
+            return timesheetUpdatedData
+        } else {
+            return t;
+        }
+    })
+    fs.writeFile('src/data/time-sheets.json', JSON.stringify(timesheetsUpdated), (err) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send('Timesheet updated');
+        }
+    })
+});
+
+router.delete('/:id', (req, res) => {
+    const timesheetId = req.params.id;
+    const filteredTimesheets = timesheets.filter((t) => t.id !== timesheetId);
+    if (timesheets.length === filteredTimesheets.length) {
+      res.send('Could not delete timesheet, not found');
+    } else {
+      fs.writeFile('src/data/time-sheets.json', JSON.stringify(filteredTimesheets), (err) => {
+        if (err) {
+          res.send(err);
+        } else {
+          res.send('Timesheets deleted');
+        }
+      });
+    }
+  });
+
+export default router;
