58b502f3dce11e196af47b6b8fb10f4ff50d6d3b
OctavioRodriguezPaseyro <101280747+OctavioRodriguezPaseyro@users.noreply.github.com>
2022-05-10 20:55:34 -0300

    TG: final corrections

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 // use 'import' to import libraries
 import express from 'express';
+import admins from './data/admins.json';
+import timesheets from './data/time-sheets.json';
 import timesheetRouter from './resources/time-sheets';
+
 // use 'require' to import JSON files
-const timesheets = require('./data/time-sheets.json');
-const admins = require('./data/admins.json');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,19 +12,19 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use('/timesheets', timesheetRouter);
 
-app.get('/', async (req, res) => {
-  res.send('Hello World!');
+// app.get('/', async (req, res) => {
+//   res.send('Hello World!');
+// });
+
+app.get('/admins', (req, res) => {
+  res.status(200).json({
+    data: admins,
+  });
 });
 
 app.get('/timesheets', (req, res) => {
   res.status(200).json({
     data: timesheets,
-  });
-});
-
-app.get('/admins', (req, res) => {
-  res.status(200).json({
-    data: admins,
   });
 });
 
