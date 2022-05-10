390d3c607172f9df4b22bfd7a567607b0d84b1dc
andreaduarte014 <andreaduarte014@gmail.com>
2022-05-10 16:01:11 -0300

    TG: lint fixed errors

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 // use "import" to import libraries
-import express from "express"
+import express from 'express';
 
 // use "require" to import JSON files
-const admins = require('./data/admins.json');
-const users = require('./data/super-admins.json')
 const superadminsRouter = require('./resources/super-admins');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use('/superadmins',superadminsRouter);
+app.use('/superadmins', superadminsRouter);
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(`Example app listening on port ${port}`);
-})
+});
