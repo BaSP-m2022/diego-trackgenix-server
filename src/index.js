ab87c8376c248e78c7d0bfa4d688dd0ba6a4a8ca
Paula Rinaldi <paurinaldi95@gmail.com>
2022-05-10 22:18:25 -0300

    Deleted unnecessary app.get route and logic on index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 // use "import" to import libraries
 import express from 'express';
 import projectsRouter from './resources/projects';
-import projects from './data/projects.json';
 
 // use "require" to import JSON files
 const admins = require('./data/admins.json');
@@ -19,12 +18,6 @@ app.get('/', async (req, res) => {
 app.get('/admins', (req, res) => {
   res.status(200).json({
     data: admins,
-  });
-});
-
-app.get('/projects', (req, res) => {
-  res.status(200).json({
-    data: projects,
   });
 });
 
