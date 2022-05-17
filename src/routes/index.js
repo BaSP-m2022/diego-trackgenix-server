d6ef9e8df133634fb58cfbebc403c93457c514c8
Nicolas Corea <nicofe12@gmail.com>
2022-05-17 20:42:14 -0300

    TG-7: Admins functionalities completed

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import employeesRoutes from './employees';
 import timeSheetRoutes from './timesheetRoutes';
-
+import adminsRoutes from './admins';
 const router = express.Router();
 
 router
   .use('/timesheet', timeSheetRoutes)
-  .use('/employees', employeesRoutes);
+  .use('/employees', employeesRoutes)
+  .use('/admins', adminsRoutes);
 
 export default router;
