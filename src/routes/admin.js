fd3ff111d172d4b5e3fe8a9cd188271eae42e697
OctavioRodriguezPaseyro <101280747+OctavioRodriguezPaseyro@users.noreply.github.com>
2022-05-17 21:03:30 -0300

    TG-7: Admins functionalities and postman collection

---
+++ src/routes/admin.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import adminsController from '../controllers/admins';
+import adminValidation from '../validations/admins';
+
+const router = express.Router();
+
+router
+  .get('/', adminsController.getAdmin)
+  .get('/:id', adminsController.getAdminById)
+  .post('/', adminValidation.adminValidation, adminsController.createAdmin)
+  .put('/:id', adminValidation.adminValidation, adminsController.updateAdmin)
+  .delete('/:id', adminsController.deleteAdmin)
+
+export default router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import employeesRoute from './employees';
+import employeesRoutes from './employees';
 import timeSheetRoutes from './timesheetRoutes';
-
+import adminsRoutes from './admins';
 const router = express.Router();
 
 router
-  .use('/employees', employeesRoute)
-  .use('/timesheet', timeSheetRoutes);
+  .use('/timesheet', timeSheetRoutes)
+  .use('/employees', employeesRoutes)
+  .use('/admins', adminsRoutes);
 
 export default router;
