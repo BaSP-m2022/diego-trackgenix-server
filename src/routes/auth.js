2a1ecaec60f650f7971975d0c17490b676c3d9f4
Agustin <alchazarreta0@gmail.com>
2022-06-27 19:51:06 -0300

    BaSP-TG-103: implement setup for firebase server

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,11 +2,13 @@ import express from 'express';
 // import controllers from '../controllers/auth';
 // import validations from '../validations/auth';
 // import authMiddleware from '../middlewares/authMiddleware';
+// import login from '../controllers/auth';
 
 const router = express.Router();
 // const { register } = controllers;
 // router.post('/register', validations.required, register);
 // router.post('/login', validations.required, login);
+
 // router.post('/logout', authMiddleware, logout);
 
 export default router;
--- src/validations/auth.js
+++ src/validations/auth.js
@@ -1,7 +1,7 @@
 const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 const required = (req, res, next) => {
-  console.log('req: ', req.body);
+  console.log(req.body);
   if (!req.body.email || !req.body.email.match(emailRegex) || !req.body.password) {
     return res.status(400).send({ message: 'Authentification error' });
   }
